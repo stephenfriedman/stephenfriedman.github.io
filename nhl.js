9e015c6087bef33dc351703c34cba7b9b917ec27
Stephen Friedman <stephen.friedman@deciphernow.com>
2020-01-28 10:05:37 -0500

    request timing modified so it actually refreshes

--- nhl.js
+++ nhl.js
@@ -3,29 +3,36 @@ var url = 'https://nhl-score-api.herokuapp.com/api/scores/latest';
 
 Http.open("GET", url);
 Http.send();
-
+setInterval(refresh, 8000)
+function refresh() {
+    Http.open("GET", url);
+    Http.send();
+    console.log(JSON.parse(Http.responseText))
+}
 showGames = 6
 currentSpot = 0
-interval = 20000
-
+interval = 10000
+var tally = 0
 setInterval(update, interval);
-    function update() {
-    var allGames = JSON.parse(Http.responseText)["games"];
-    if (allGames.length <= 0) {
+
+function update() {
+    console.log(tally)
+    tally++
+    var games = JSON.parse(Http.responseText)["games"];
+    console.log(games)
+    if (games.length <= 0) {
         console.log("0 games")
-        interval = 10000
         var i;
-        for(i=1;i<7;i++){
-        document.getElementById("awayTeam" + i.toString()).innerHTML = "---";
-        document.getElementById("homeTeam" + i.toString()).innerHTML = "---";
-        document.getElementById("awayScore" + i.toString()).innerHTML = "-";
-        document.getElementById("homeScore" + i.toString()).innerHTML = "-";
-        document.getElementById("timeRemaining" + i.toString()).innerHTML = "0:00";
-        document.getElementById("periodNum" + i.toString()).innerHTML = "-";
+        for (i = 1; i < 7; i++) {
+            document.getElementById("awayTeam" + i.toString()).innerHTML = "---";
+            document.getElementById("homeTeam" + i.toString()).innerHTML = "---";
+            document.getElementById("awayScore" + i.toString()).innerHTML = "-";
+            document.getElementById("homeScore" + i.toString()).innerHTML = "-";
+            document.getElementById("timeRemaining" + i.toString()).innerHTML = "0:00";
+            document.getElementById("periodNum" + i.toString()).innerHTML = "-";
         }
         return
     }
-    var games = JSON.parse(allGames)["games"];
     var numGames = Object.keys(games).length
     if (numGames <= 0) {
         return
@@ -33,7 +40,7 @@ setInterval(update, interval);
 
     for (i = 0; i < showGames; i++) {
         if (currentSpot >= numGames) {
-        currentSpot = 0
+            currentSpot = 0
         }
         getScores(currentSpot, games, i)
         currentSpot += 1;
@@ -69,22 +76,25 @@ function getScores(currentSpot, games, position) {
     // Time and Period
     if (game["status"]["state"] === "LIVE") {
         timeRemaining = game["status"]["progress"]["currentPeriodTimeRemaining"]["pretty"]
-        period = game["status"]["progress"]["currentPeriodOrdinal"]
+        periodNum = game["status"]["progress"]["currentPeriodOrdinal"]
+        if(timeRemaining == "END"){
+            timeRemaining = timeRemaining+ " " + periodNum
+        }
     } else if (game["status"]["state"] === "FINAL") {
         timeRemaining = "FINAL"
-        period = "---"
+        periodNum = "---"
     } else if (game["status"]["state"] === "PREVIEW") {
-        timeRemaining = "20:00"
-        period = "1st"
+        timeRemaining = "--:--"
+        periodNum = "1st"
     } else {
         timeRemaining = "Error"
-        period = "Err"
+        periodNum = "Err"
     }
 
     position = position + 1
     var posStr = position.toString()
-    document.getElementById("awayLogo" + posStr).src = "logos/"+awayTeam+".svg"; 
-    document.getElementById("homeLogo" + posStr).src = "logos/"+homeTeam+".svg"; 
+    document.getElementById("awayLogo" + posStr).src = "logos/" + awayTeam + ".svg";
+    document.getElementById("homeLogo" + posStr).src = "logos/" + homeTeam + ".svg";
     document.getElementById("awayTeam" + posStr).innerHTML = awayTeam;
     document.getElementById("homeTeam" + posStr).innerHTML = homeTeam;
     document.getElementById("awayScore" + posStr).innerHTML = awayScore;
@@ -92,13 +102,16 @@ function getScores(currentSpot, games, position) {
     document.getElementById("timeRemaining" + posStr).innerHTML = timeRemaining;
     document.getElementById("periodNum" + posStr).innerHTML = periodNum;
 
-    var periodNum = timeRemaining = game["status"]["progress"]["currentPeriod"]
-    if(periodNum>3){
-        document.getElementById("timeBox" + posStr).style.backgroundColor = "#962715"
+    if (game["status"]["state"] === "LIVE") {
+        var periodNum = game["status"]["progress"]["currentPeriod"]
+        if (periodNum > 3) {
+            document.getElementById("timeBox" + posStr).style.backgroundColor = "#962715"
+        }
+
+        var minsRemaining = game["status"]["progress"]["currentPeriodTimeRemaining"]["min"]
+        if (Math.abs(awayScore - homeScore) <= 1 && periodNum == 3 && minsRemaining <= 5) {
+            document.getElementById("timeBox" + posStr).style.backgroundColor = "#FF8D3F"
+        }
     }
 
-    var minsRemaining = game["status"]["progress"]["currentPeriodTimeRemaining"]["min"]
-    if(Math.abs(awayScore-homeScore)<=1 && periodNum==3 && minsRemaining <= 5){
-        document.getElementById("timeBox" + posStr).style.backgroundColor = "#FF8D3F"
-    }
 }
